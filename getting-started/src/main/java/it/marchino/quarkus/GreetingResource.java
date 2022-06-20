47109dae9854fc8d1af734449c0bfcbdb3dfa48b
amarchino <marchino.alessandro@gmail.com>
2022-06-20 23:22:33 +0200

    Lesson 10

--- getting-started/src/main/java/it/marchino/quarkus/GreetingResource.java
+++ getting-started/src/main/java/it/marchino/quarkus/GreetingResource.java
@@ -1,46 +1,22 @@
 package it.marchino.quarkus;
 
-import javax.ws.rs.Consumes;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import it.marchino.quarkus.service.GreetingService;
+
 @Path("/hello")
 public class GreetingResource {
-	public static final String TOKEN = "dev";
+	@Inject private GreetingService greetingService;
 
 	@GET
-	@Path("/html")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String hello() {
-		return "Hello from <b>RESTEasy Reactive</b>";
+	public String hello(@QueryParam("name") String name) {
+		return greetingService.sayHello(name);
 	}
 
-	@GET
-	@Path("/html/{id}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String hello(@PathParam("id") String id) {
-		return "Hello " + id;
-	}
-
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String addHello(
-			@HeaderParam("name") String hName,
-			@QueryParam("name") String aName,
-			String body) {
-		String token = hName != null ? hName : aName;
-		if(!TOKEN.equals(token)) {
-			throw new RuntimeException("Unauthorized");
-		}
-		// return "<b>Text<b>";
-		return "{\"key\": \"" + token + "\"}";
-		// return body;
-	}
 }
---
+++ getting-started/src/main/java/it/marchino/quarkus/service/GreetingService.java
@@ -0,0 +1,13 @@
+package it.marchino.quarkus.service;
+
+import java.util.UUID;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+	public String sayHello(String name) {
+		return String.format("Hello %s, your id is %s", name, UUID.randomUUID());
+	}
+}
